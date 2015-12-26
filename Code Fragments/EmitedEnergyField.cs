402cbc12757518ead664ab59b6d05f7daa95dfac
Keyarose <myst600@hotmail.com>
2015-12-26 18:04:44 -0400

    Additions to EEF

--- Code Fragments/EmitedEnergyField.cs
+++ Code Fragments/EmitedEnergyField.cs
@@ -3,6 +3,7 @@ namespace KirosPhysics
     public class EmitedEnergyField : EnergyField
     {
         protected Vector3 _emissionPoint;
+        protected float _fieldStrength; //defines a multiplyer for the strength of the field
         
         public Vector3 EmissionPoint
         {
@@ -14,7 +15,9 @@ namespace KirosPhysics
         
         public override float StrengthAtPoint(Vector3 point)
         {
-        
+            //options: compile at runtime, string parseing, script engine (best option) 
         }
+        
+        //TODO: define material and other field interactions
     }
 }
