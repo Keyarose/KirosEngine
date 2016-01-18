ddeb0a100a1c7c05baed558a92960bfc6c72cb2d
Keyarose <myst600@hotmail.com>
2016-01-18 13:26:25 -0400

    Addition of ActorMemory, and additions to Event

---
+++ Code Fragments/ActorMemory.cs
@@ -0,0 +1,49 @@
+namespace KirosProject
+{
+    public class ActorMemory
+    {
+        protected List<Event> _eventList;
+        protected string _actorID; //owning actor's id
+        
+        public string OwnerID
+        {
+            get
+            {
+                return _actorID;
+            }
+        }
+        
+        public ActorMemory(string actorID)
+        {
+            _actorID = actorID;
+            _eventList = new List<Event>();
+        }
+        
+        public void AddEvent(Event event)
+        {
+            _eventList.Add(event);
+        }
+        
+        //returns null if not found?
+        public Event GetEvent(string eventID)
+        {
+            return _eventList.Find(x => x.ID.Equals(eventID));
+        }
+        
+        //returns empty list if not found
+        public List<Event> GetEventsInvolvingActor(string actorID)
+        {
+            List<Event> result = new List<Event>();
+            
+            foreach(Event e in _eventList)
+            {
+                if(e.InvolvesActor(actorID))
+                {
+                    result.Add(e);
+                }
+            }
+            
+            return result;
+        }
+    }
+}
--- Code Fragments/Event.cs
+++ Code Fragments/Event.cs
@@ -3,9 +3,12 @@ namespace KirosProject
     class Event
     {
         protected string _id;
+        protected string _masterID;
+        protected TimeCode _time; //defines when the event was in terms of the world
         //list of actions grouped with the performing actor's id and an unique id number
         protected List<Triad<uint, string, Action>> _actorActionList;
         //string == "null" refers to unknown actor
+        protected uint _nextActionID;
         
         public string ID
         {
@@ -15,10 +18,27 @@ namespace KirosProject
             }
         }
         
+        public string MasterID
+        {
+            get
+            {
+                return _masterID;
+            }
+        }
+        
+        public int ActionCount
+        {
+            get
+            {
+                return _actorActionList.Count();
+            }
+        }
+        
         public Event(string id)
         {
             _id = id;
-            _actorActionList = new List<Pair<string, Action>>();
+            _actorActionList = new List<Triad<uint, string, Action>>();
+            _nextActionID = 0;
         }
         
         ///<summary>
@@ -28,7 +48,8 @@ namespace KirosProject
         ///<param name="action">The action being added</param>
         public void AddAction(string actorID, Action action)
         {
-            _actorActionList.Add(new Pair<string, Action>(actorID, action));
+            _actorActionList.Add(new Triad<uint, string, Action>(_nextActionID, actorID, action));
+            _nextActionID++;
         }
         
         ///<summary>
@@ -38,9 +59,25 @@ namespace KirosProject
         {
             List<Actor> result = new List<Actor>();
             
-            foreach(Pair<string, Action> p in _actorActionList)
+            foreach(Triad<uint, string, Action> p in _actorActionList)
             {
-                result.add(ActorManager.GetActor(p.Item1));
+                result.add(ActorManager.GetActor(p.Item2));
+            }
+            
+            return result;
+        }
+        
+        public bool InvolvesActor(string actorID)
+        {
+            bool result false;
+            
+            foreach(Triad<uint, string, Action> t in _actorActionList)
+            {
+                if(t.Item2.Equals(actorID))
+                {
+                    result = true;
+                    break;
+                }
             }
             
             return result;
